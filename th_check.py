fbff54fc063ced8807f4b0e0fff0935c4c144a32
MasaGratoR <masagrator@gmail.com>
2026-01-15 09:23:00 +0100

    Update th_check.py

--- th_check.py
+++ th_check.py
@@ -1,11 +1,25 @@
 import json
 import requests
 from concurrent.futures import ThreadPoolExecutor
+import sys
 
 region = ""
 
-OUTPUT = {}
+def checkTitleid(titleid: str):
+    url = f"https://ec.nintendo.com/apps/{titleid}/TH"
+    with requests.head(url, stream=True, allow_redirects=False) as response:
+        status_code = response.status_code
+        if (status_code == 303):
+            OUTPUT[f"{titleid}"] = True
+            print(f"✓ {titleid}")
+        elif (status_code == 403):
+            print("✗ Hit rate limit. Aborting...")
+            sys.exit(1)
+        else: 
+            print(f"✗ {titleid}: {status_code}")
+            OUTPUT[f"{titleid}"] = False
     
+
 with open("output/main_regions.json", "r", encoding="UTF-8") as f:
     titleids = list(json.load(f).keys())
 
@@ -13,22 +27,14 @@ try:
     with open("output/main_regions_th.json", "r", encoding="UTF-8") as f:
         OUTPUT = json.load(f)
 except:
-    pass
+    OUTPUT = {}
 
 keys = list(OUTPUT.keys())
 
 titleids[:] = [x for x in titleids if x not in keys]
 
-for titleid in titleids:
-    url = f"https://ec.nintendo.com/apps/{titleid}/TH"
-    with requests.head(url, stream=True, allow_redirects=False) as response:
-        status_code = response.status_code
-        if (status_code == 303):
-            OUTPUT[f"{titleid}"] = True
-            print(f"✓ {titleid}")
-        else: 
-            print(f"✗ {titleid}: {status_code}")
-            OUTPUT[f"{titleid}"] = False
+with ThreadPoolExecutor(max_workers=1) as executor:
+    executor.map(checkTitleid, titleids)
 
 with open("output/main_regions_th.json", "w", encoding="UTF-8") as f:
     json.dump(OUTPUT, f)
