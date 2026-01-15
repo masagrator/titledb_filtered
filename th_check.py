eebf565b6502cf66596c856d80cb22a869d5fc4c
MasaGratoR <masagrator@gmail.com>
2026-01-15 05:54:35 +0100

    Update th_check.py

--- th_check.py
+++ th_check.py
@@ -1,8 +1,7 @@
 import json
 import requests
 from concurrent.futures import ThreadPoolExecutor
-
-region = ""
+import sys
 
 OUTPUT = []
 
@@ -13,12 +12,15 @@ def checkTitleid(titleid: str):
         if (status_code == 303):
             OUTPUT.append(titleid)
             print(f"✓ {titleid}")
+        elif (status_code == 403):
+            print("✗ Hit rate limit, aborting...")
+            sys.exit(1)
         else: print(f"✗ {titleid}: {status_code}")
 
 with open("output/main_regions.json", "r", encoding="UTF-8") as f:
     titleids = list(json.load(f).keys())
 
-with ThreadPoolExecutor(max_workers=2) as executor:
+with ThreadPoolExecutor(max_workers=1) as executor:
     executor.map(checkTitleid, titleids)
 
 with open("output/main_regions_th.json", "w", encoding="UTF-8") as f:
